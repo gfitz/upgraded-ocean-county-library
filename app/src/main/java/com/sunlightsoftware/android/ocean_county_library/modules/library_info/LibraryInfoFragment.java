f8761bed41392e515695ec6ebba443861bdbfca9
Garrett Fitzgerald <garrett.fitzgerald@appetizermobile.com>
2017-01-10 19:56:00 -0500

    wired up every textview

--- app/src/main/java/com/sunlightsoftware/android/ocean_county_library/modules/library_info/LibraryInfoFragment.java
+++ app/src/main/java/com/sunlightsoftware/android/ocean_county_library/modules/library_info/LibraryInfoFragment.java
@@ -1,12 +1,10 @@
 package com.sunlightsoftware.android.ocean_county_library.modules.library_info;
 
-import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +15,6 @@ import android.widget.TextView;
 import com.sunlightsoftware.android.ocean_county_library.Constant;
 import com.sunlightsoftware.android.ocean_county_library.R;
 import com.sunlightsoftware.android.ocean_county_library.components.utils.ImageUtils;
-import com.sunlightsoftware.android.ocean_county_library.modules.library_locator.LibraryLocatorFragment;
 
 /**
  * Created by Garrett on 1/10/2017.
@@ -30,7 +27,15 @@ public class LibraryInfoFragment extends Fragment {
     private static final String ARG_LIBRARY_NAME = "library_name";
 
     private ImageView mLibraryImageView;
-    private TextView mLibraryNameTextView;
+    private TextView mCallTextView;
+    private TextView mDirectionsTextView;
+    private TextView mSundayHoursTextView;
+    private TextView mMondayHoursTextView;
+    private TextView mTuesdayHoursTextView;
+    private TextView mWednesdayHoursTextView;
+    private TextView mThursdayHoursTextView;
+    private TextView mFridayHoursTextView;
+    private TextView mSaturdayTextView;
 
     public static LibraryInfoFragment newInstance(String photoAssetPath, String libraryName) {
         Bundle args = new Bundle();
@@ -66,6 +71,42 @@ public class LibraryInfoFragment extends Fragment {
                                 mLibraryImageView.getWidth(), mLibraryImageView.getHeight()));
             }
         });
+
+        mCallTextView = (TextView) v.findViewById(R.id.library_info_call_text_view);
+        mCallTextView.setTypeface(Typeface.createFromAsset(getContext().getAssets(),
+                Constant.MONTSERRAT_FONT));
+
+        mDirectionsTextView = (TextView) v.findViewById(R.id.library_info_directions_text_view);
+        mDirectionsTextView.setTypeface(Typeface.createFromAsset(getContext().getAssets(),
+                Constant.MONTSERRAT_FONT));
+
+        mSundayHoursTextView = (TextView) v.findViewById(R.id.sunday_hours_text_view);
+        mSundayHoursTextView.setText(getString(R.string.sunday_hours, Integer.toString(9),
+                Integer.toString(9)));
+
+        mMondayHoursTextView = (TextView) v.findViewById(R.id.monday_hours_text_view);
+        mMondayHoursTextView.setText(getString(R.string.monday_hours, Integer.toString(9),
+                Integer.toString(9)));
+
+        mTuesdayHoursTextView = (TextView) v.findViewById(R.id.tuesday_hours_text_view);
+        mTuesdayHoursTextView.setText(getString(R.string.tuesday_hours, Integer.toString(9),
+                Integer.toString(9)));
+
+        mWednesdayHoursTextView = (TextView) v.findViewById(R.id.wednesday_hours_text_view);
+        mWednesdayHoursTextView.setText(getString(R.string.wednesday_hours, Integer.toString(9),
+                Integer.toString(9)));
+
+        mThursdayHoursTextView = (TextView) v.findViewById(R.id.thursday_hours_text_view);
+        mThursdayHoursTextView.setText(getString(R.string.thursday_hours, Integer.toString(9),
+                Integer.toString(9)));
+
+        mFridayHoursTextView = (TextView) v.findViewById(R.id.friday_hours_text_view);
+        mFridayHoursTextView.setText(getString(R.string.friday_hours, Integer.toString(9),
+                Integer.toString(9)));
+
+        mSaturdayTextView = (TextView) v.findViewById(R.id.saturday_hours_text_view);
+        mSaturdayTextView.setText(getString(R.string.saturday_hours, Integer.toString(9),
+                Integer.toString(9)));
 
         return v;
     }
