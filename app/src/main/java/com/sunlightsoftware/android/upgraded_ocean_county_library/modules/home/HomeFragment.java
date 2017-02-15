469f6721747e9971f40c5b2af6c21c8da809c16b
Garrett Fitzgerald <garrett.fitzgerald@appetizermobile.com>
2017-02-15 16:56:39 -0500

    removed all interstial ads bc they loaded slow

--- app/src/main/java/com/sunlightsoftware/android/upgraded_ocean_county_library/modules/home/HomeFragment.java
+++ app/src/main/java/com/sunlightsoftware/android/upgraded_ocean_county_library/modules/home/HomeFragment.java
@@ -39,7 +39,6 @@ public class HomeFragment extends Fragment {
     private HomeButton mMyAccountButton;
     private HomeButton mLibraryLocatorButton;
     private HomeButton mSearchTheCatalogButton;
-    private InterstitialAd mInterstitialAd;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -93,77 +92,13 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                boolean loaded = mInterstitialAd.isLoaded();
-                if (loaded) {
-                    mInterstitialAd.show();
-                } else {
-                    Intent i = LibraryLocatorActivity.newIntent(getActivity());
-                    startActivity(i);
-                }
+                Intent i = LibraryLocatorActivity.newIntent(getActivity());
+                startActivity(i);
+
             }
         });
-
-        initializeInterstitialAd();
 
         return v;
     }
-
-    // Initializes the Intersitial Ad
-    private void initializeInterstitialAd() {
-        mInterstitialAd = new InterstitialAd(getContext());
-        mInterstitialAd.setAdUnitId("ca-app-pub-7195172863816354/7807319023");
-
-        mInterstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                requestNewInterstitialAd();
-                Intent i = LibraryLocatorActivity.newIntent(getActivity());
-                startActivity(i);
-            }
-        });
-
-        requestNewInterstitialAd();
-    }
-
-    //Loads a brand new ad into mIntersitialAd
-    private void requestNewInterstitialAd() {
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .addTestDevice(Settings.Secure.getString(getContext().getContentResolver(),
-                        Settings.Secure.ANDROID_ID))
-                .build();
-        mInterstitialAd.loadAd(adRequest);
-    }
-
-    class LoadBannerImageTask extends AsyncTask<Integer, Void, Bitmap> {
-        private final WeakReference<ImageView> imageViewWeakReference;
-        private int data = 0;
-        private int reqWidth;
-        private int reqHeight;
-
-        public LoadBannerImageTask(ImageView imageView, int reqWidth, int reqHeight) {
-            imageViewWeakReference = new WeakReference<ImageView>(imageView);
-            this.reqWidth = reqWidth;
-            this.reqHeight = reqHeight;
-        }
-
-        @Override
-        protected Bitmap doInBackground(Integer... integers) {
-            data = integers[0];
-            return ImageUtils.decodeSampledBitmapFromResource(getResources(), data,
-                    reqWidth, reqHeight);
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            if (imageViewWeakReference != null && bitmap != null) {
-                final ImageView imageView = imageViewWeakReference.get();
-                if (imageView != null)
-                    imageView.setImageBitmap(bitmap);
-            }
-
-        }
-    }
-
 
 }
